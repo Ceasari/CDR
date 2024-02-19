f386a422c5d50e7535de6a2bd70cc1b206e8a6ec
Ceasar <sergey-iline@mail.ru>
2024-02-20 08:55:20 +1300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-API_TOKEN = '6201876821:AAHKMdoykAOtKGXKZnTFnt1OiixAMLlCZsE'
+API_TOKEN = ''
 ALLOWED_FORMATS = ["jpeg", "jpg", "png", "gif"]
 MODEL_TYPE = "resnet"
 MODEL_PATH = "Model/yolo/best.pt"
